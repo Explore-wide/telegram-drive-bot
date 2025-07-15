96c761d93cfbec44e802ebcce1ced37fd3175279
Explore-wide <156446285+Explore-wide@users.noreply.github.com>
2025-07-15 09:13:39 -0700

    Update bot-drive.py

--- bot-drive.py
+++ bot-drive.py
@@ -94,5 +94,6 @@ def set_webhook():
 # Uncomment this line to set the webhook when the bot starts
 # set_webhook()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if name == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
